1e94c6d7aa34cd7c21573bdf8e4fc37d53e977f4
Jarbas de Moraes <jarbasmoraes@gmail.com>
2023-02-13 07:51:12 -0300

    Adicionado integração: Amplify studio + figma

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import {QRCodeGenerator} from "./features/qrcode/QRCodeGenerator";
+import {NavBar} from "./ui-components";
 
 function App() {
 
   return (
     <div className="App">
+
+      <NavBar />
       <QRCodeGenerator/>
 
     </div>
